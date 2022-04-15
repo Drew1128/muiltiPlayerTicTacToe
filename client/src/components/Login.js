caeee9c8348e9a0326ce99c6a60c4dea3aab30f4
Drew Morrison <drewm1128@gmail.com>
2022-04-15 09:55:42 -0500

    boilerplate code for the login component done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
 const Login = () => {
+    
+
+    const signUp = () => {
+        return null
+    }
+
+   
   return (
-    <div>Login</div>
-  )
+    <div className='login'>
+        <label>Login Up</label>
+        <input 
+        type='text' 
+        placeholder='First Name' 
+        onChange={(event) => {
+            setUser({...user, firstName: event.target.value})
+        }} />
+        <input 
+        type='text' 
+        placeholder='Last Name' 
+        onChange={(event) => {
+            setUser({...user, lastName: event.target.value})
+        }} />
+        <input 
+        type='text' 
+        placeholder='Username' 
+        onChange={(event) => {
+            setUser({...user, username: event.target.value})
+        }} />
+        <input 
+        type='text' 
+        placeholder='Password' 
+        onChange={(event) => {
+            setUser({...user, password: event.target.value})
+        }} />
+        <button 
+        onClick={() => {
+            signUp()   
+        } }
+        >Login</button>
+    </div>
+    )
 }
 
 export default Login
